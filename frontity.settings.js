8b707fb8dc337c6abca7c113e5e3aa4b0639dd91
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-04-22 15:25:03 +0545

    Initial Commit

---
+++ frontity.settings.js
@@ -0,0 +1,42 @@
+const settings = {
+  name: "sawtee-frontend",
+  state: {
+    frontity: {
+      url: "https://test.frontity.org",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development",
+    },
+  },
+  packages: [
+    {
+      name: "sawtee-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Nature", "/category/nature/"],
+            ["Travel", "/category/travel/"],
+            ["Japan", "/tag/japan/"],
+            ["About Us", "/about-us/"],
+          ],
+          featured: {
+            showOnList: true,
+            showOnPost: true,
+          },
+        },
+      },
+    },
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://test.frontity.org",
+        },
+      },
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+  ],
+};
+
+export default settings;
