d78de0e3f24864a5079debc37acdcddc096598c3
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-06-01 17:32:06 +0545

    working on slider: push from ofc desktop

--- packages/sawtee-theme/src/components/slider/slider.js
+++ packages/sawtee-theme/src/components/slider/slider.js
@@ -1,48 +1,187 @@
 // Import Swiper React components
 import { styled } from "frontity";
-import { Carousel } from "@sefailyasoz/react-carousel";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { useSwipeable } from "react-swipeable";
+import { HiArrowNarrowLeft, HiArrowNarrowRight } from "react-icons/hi";
+import { BsDot } from "react-icons/bs";
+
+const Slide = ({ slides, width, updateIndex, activeIndex }) => {
+  return (
+    <>
+      {slides?.map((slide, i) => {
+        return (
+          <SliderItem key={i} width={width}>
+            <SliderImage src={slide.imgPath} alt={slide.title} />
+            <div className={styles.content}>
+              <p className={styles.title}>{slide.title}</p>
+              <p className={styles.subtitle}>{slide.subtitle}</p>
+            </div>
+            <div className={cn(styles.navigation)}>
+              <ul className={styles.list}>
+                {slides?.map((slide, i) => {
+                  return (
+                    <li
+                      key={i}
+                      className={`${
+                        i === activeIndex ? styles.active : styles.listItem
+                      }`}
+                    >
+                      <span
+                        className="trigger cursor-pointer"
+                        onClick={() => {
+                          updateIndex(i);
+                        }}
+                      >
+                        {slide.title}
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </SliderItem>
+        );
+      })}
+    </>
+  );
+};
 
 const Slider = ({ slides }) => {
-  const CarouselData = [
-    {
-      headerText: null,
-      subText: "Sub Text One",
-      image: "https://picsum.photos/300/300",
-    },
-    {
-      headerText: "Header Text Two",
-      subText: null,
-      image: "https://picsum.photos/1200/800",
-    },
-    {
-      headerText: null,
-      subText: null,
-      image: "https://picsum.photos/720/720",
-    },
-    {
-      headerText: "Header Text Four",
-      subText: "Sub Text Four",
-      image: "https://picsum.photos/1920/1080",
-    },
-    {
-      headerText: "Header Text Five",
-      subText: "Sub Text Five",
-      image: "https://picsum.photos/480/360",
-    },
-  ];
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = slides.length - 1;
+    } else if (newIndex > slides.length - 1) {
+      newIndex = 0;
+    }
+    setActiveIndex(newIndex);
+    setActiveImageLink(slides[newIndex].imgPath);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!paused && activeIndex < slides.length - 1) {
+        updateIndex(activeIndex + 1);
+      } else if (!paused && activeIndex === slides.length - 1) {
+        updateIndex(0);
+      }
+    }, 3000);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  });
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => updateIndex(activeIndex + 1),
+    onSwipedRight: () => updateIndex(activeIndex - 1),
+  });
+
   return (
-    <Carousel
-      data={CarouselData}
-      autoPlay={true}
-      rightItem={<FaArrowRight />}
-      leftItem={<FaArrowLeft />}
-      animationDuration={3000}
-      headerTextType="black"
-      subTextType="white"
-      size="normal"
-    />
+    <div
+      {...handlers}
+      className={cn(styles.container)}
+      style={{ backgroundColor: Hex }}
+    >
+      <div
+        className={styles.wrapper}
+        onMouseEnter={() => {
+          setPaused(true);
+        }}
+        onMouseLeave={() => {
+          setPaused(false);
+        }}
+      >
+        <div
+          className={styles.inner}
+          style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+        >
+          <Slide
+            slides={slides}
+            width="100%"
+            updateIndex={updateIndex}
+            activeIndex={activeIndex}
+          />
+        </div>
+      </div>
+      <div className={styles.indicators}>
+        <button
+          onClick={() => {
+            if (activeIndex <= 0) {
+              updateIndex(slides.length - 1);
+            } else if (activeIndex <= slides.length - 1) {
+              updateIndex(activeIndex - 1);
+            }
+          }}
+        >
+          <HiArrowNarrowLeft />
+        </button>
+        <button
+          onClick={() => {
+            if (activeIndex === slides.length - 1) {
+              updateIndex(0);
+            } else {
+              updateIndex(activeIndex + 1);
+            }
+          }}
+        >
+          <HiArrowNarrowRight />
+        </button>
+      </div>
+
+      <ul>
+        {slides?.map((slide, i) => {
+          return (
+            <li
+              key={i}
+              className={`${i === activeIndex ? "active" : "text-gray-300"}`}
+            >
+              <span
+                className="cursor-pointer shadow-lg"
+                onClick={() => {
+                  updateIndex(i);
+                }}
+              >
+                <BsDot />
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
 export default Slider;
+
+const Sliderr = styled.div`
+  overflow: hidden;
+`;
+
+const SliderInner = styled.div`
+  white-space: nowrap;
+  transition: transform 0.3s;
+`;
+
+const SliderItem = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  color: white;
+`;
+
+const SliderImage = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  opacity: 0.7;
+  filter: alpha(opacity=70);
+  & :hover {
+    opacity: 0.9;
+    filter: alpha(opacity=90);
+  }
+`;
