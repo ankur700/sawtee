d0319872cc753ea3bf5085bf6933a2690cdcaadd
Ankur Singh <abishek.singh.chauhan@gmail.com>
2023-01-13 17:26:28 +0545

    reasearch page updated.

--- packages/custom-theme/src/pages/research/researchList.js
+++ packages/custom-theme/src/pages/research/researchList.js
@@ -1,16 +1,19 @@
 import * as React from "react";
 import {
-  Box,
-  HStack,
   VStack,
+  Heading,
+  Box,
+  Link,
+  Container,
+  Circle,
+  Flex,
   Text,
-  Tag,
-  Image,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { connect, decode } from "frontity";
 import { getCPTData } from "../../components/helpers";
-import Link from "../../components/atoms/link";
+import link from "../../components/atoms/link";
+import { HiOutlineNewspaper } from "react-icons/hi";
 
 const ResearchList = ({ state, link, linkColor }) => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -27,20 +30,42 @@ const ResearchList = ({ state, link, linkColor }) => {
   };
   const color = linkColor;
   const researchlist = getCPTData(posts(), state);
+  // console.log(researchlist[0].tags);
 
-  const researchData = new Set();
+  const tagsArray = () => {
+    let array = [];
+    researchlist.forEach(({ tags }) => {
+      tags.map((tag) => array.push({ id: tag.id, name: tag.name, posts: [] }));
+    });
+    if (array.length > 0) {
+      return array;
+    }
+  };
 
-  
+  tagsArray();
 
+  function postsSortedByTags(tag) {
+    researchlist.map((r) =>
+      r.tags.map((t) => {
+        if (tag.id === t.id) {
+          tag.posts.push(r);
+        }
+      })
+    );
+  }
 
   return (
-    <>
-      <VStack spacing={4}>
-        {researchlist.map(
-          ({ id, title, featured_media, link, tags, categories }) => (
-            <Box onClick={toggleOpen} w="full" key={id}>
-              <HStack
-                // p={4}
+    <Container maxW="7xl" p={{ base: 2, sm: 10 }}>
+      <VStack textAlign="start" align="start" mb={5} spacing={10}>
+        {tagsArray().map((tagitem) => {
+          postsSortedByTags(tagitem);
+          return (
+            <Box zIndex={5} key={tagitem}>
+              <Heading fontSize="4xl" fontWeight="600" my={5}>
+                {tagitem.name}
+              </Heading>
+              <Box
+                p={4}
                 bg={useColorModeValue("white", "gray.800")}
                 rounded="xl"
                 borderWidth="1px"
@@ -53,79 +78,66 @@ const ResearchList = ({ state, link, linkColor }) => {
                 cursor="pointer"
                 _hover={{ shadow: "lg" }}
               >
-                {featured_media && (
-                  <Image
-                    {...featured_media}
-                    size="md"
-                    width={40}
-                    height={60}
-                    layout="fixed"
-                    border={"1px solid"}
-                    borderColor={useColorModeValue(
-                      "gray.100",
-                      "whiteAlpha.700"
-                    )}
-                    // rounded="md"
-                    objectFit="cover"
-                    fallbackSrc="https://via.placeholder.com/150"
-                  />
-                )}
-                <VStack align="start" justify="flex-start">
-                  <VStack spacing={3} align="start">
-                    <HStack spacing="1">
-                      {categories.map((cat) => (
-                        <Tag
-                          key={cat.id}
-                          colorScheme={useColorModeValue("blackAlpha", "gray")}
-                          borderRadius="full"
-                          bg={useColorModeValue(
-                            "rgb(230 247 255/1)",
-                            "rgb(88,175,223,.1)"
-                          )}
-                        >
-                          {cat.name}
-                        </Tag>
-                      ))}
-                    </HStack>
-                    <HStack>
-                      <Text
-                        color={useColorModeValue("gray.700", "whiteAlpha.700")}
-                        fontSize="xl"
-                        lineHeight={1.2}
-                        fontWeight="bold"
-                        _hover={{
-                          color: color,
-                          textDecoration: "underline",
-                        }}
-                      >
-                        <Link link={link}>{decode(title)}</Link>
-                      </Text>
-                    </HStack>
-
-                    {/* {!isOpen && (
-                        <Text
-                          fontSize="sm"
-                          color={textColor}
-                          noOfLines={{ base: 2 }}
-                        >
-                          {desc}
-                        </Text>
-                      )}
-
-                      {isOpen && (
-                        <Text fontSize="sm" color={textColor}>
-                          {desc}
-                        </Text>
-                      )} */}
-                  </VStack>
-                </VStack>
-              </HStack>
+                {tagitem.posts.map((researchItem) => (
+                  <ReasearchItem
+                    key={researchItem.id}
+                    icon={HiOutlineNewspaper}
+                    skipTrail={tagitem.posts.length > 1 ? true : false}
+                  >
+                    <Text
+                      color={useColorModeValue("gray.700", "whiteAlpha.700")}
+                      fontSize="xl"
+                      lineHeight={1.2}
+                      fontWeight="bold"
+                      _hover={{
+                        color: color,
+                        textDecoration: "underline",
+                      }}
+                    >
+                      <Link href={researchItem.link}>{researchItem.title}</Link>
+                    </Text>
+                  </ReasearchItem>
+                ))}
+              </Box>
             </Box>
-          )
-        )}
+          );
+        })}
       </VStack>
-    </>
+    </Container>
   );
 };
 
 export default connect(ResearchList);
+
+const ReasearchItem = ({
+  icon = FiCheckCircle,
+  boxProps = {},
+  skipTrail,
+  children,
+  ...props
+}) => {
+  const color = useColorModeValue("gray.700", "gray.500");
+  return (
+    <Flex minH={20} {...props}>
+      <Flex flexDir="column" alignItems="center" mr={4} pos="relative">
+        <Circle
+          size={12}
+          bg={useColorModeValue("gray.600", "gray.500")}
+          opacity={useColorModeValue(0.07, 0.15)}
+        />
+        <Box
+          as={icon}
+          size="1.25rem"
+          color={color}
+          pos="absolute"
+          left="0.875rem"
+          top="0.875rem"
+        />
+        {skipTrail && <Box w="1px" flex={1} bg={color} my={1} />}
+      </Flex>
+      <Box pt={{ base: 1, sm: 3 }} {...boxProps}>
+        {children}
+      </Box>
+    </Flex>
+  );
+};
