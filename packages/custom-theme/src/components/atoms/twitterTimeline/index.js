8efa28969aab5531213d4a3e5bd95de2cd4316c7
Ankur Singh <abishek.singh.chauhan@gmail.com>
2023-04-12 17:35:02 +0545

    twitter timeline fix

--- packages/custom-theme/src/components/atoms/twitterTimeline/index.js
+++ packages/custom-theme/src/components/atoms/twitterTimeline/index.js
@@ -1,52 +1,38 @@
-import { useColorModeValue, Box, Link } from "@chakra-ui/react";
+import { useColorModeValue, Box } from "@chakra-ui/react";
 import React from "react";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+import { styled } from "frontity";
 
 const TwitterTimeline = ({ height, width, handle }) => {
-  const link = `https://twitter.com/${handle}?ref_src=twsrc%5Etfw`;
-
   const theme = useColorModeValue("light", "dark");
 
-  React.useEffect(() => {
-    const wrapper = document.getElementById("twitter-wrapper");
-    const srcipt = document.createElement("script");
-    srcipt.setAttribute("src", "https://platform.twitter.com/widgets.js");
+  const TwitterWrapper = styled(Box)`
+    display: flex;
+    justifycontent: center;
+    alignitems: center;
+    fontweight: bold;
+    width: 100%;
 
-    const anchor = document.createElement("a");
-    anchor.dataset.width = width;
-    anchor.dataset.height = height;
-    anchor.textContent = "Track SAWTEE on Twitter";
-    anchor.setAttribute("class", "twitter-timeline");
-    anchor.setAttribute("href", link);
-    anchor.dataset.chrome = "noscrollbars noborders";
-    anchor.dataset.theme = theme;
-    wrapper.appendChild(anchor);
-    anchor.appendChild(srcipt);
-  }, []);
-
-  React.useEffect(() => {
-    const prevAnchor = document.getElementsByClassName("twitter-timeline")[0];
-    const twitterEmbed = document.getElementsByClassName(".r-kemski")[0];
-
-
-    if (prevAnchor) {
-      prevAnchor.setAttribute("data-theme", theme);
+    & div {
+      width: 100%;
     }
-
-    if (twitterEmbed) {
-      twitterEmbed.style.background = "transparent !important";
-    }
-  }, [theme]);
+  `;
 
   return (
-    <Box
-      id="twitter-wrapper"
-      padding={6}
-      display={"flex"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      fontWeight={"semibold"}
-      w="full"
-    ></Box>
+    <TwitterWrapper id="twitter-wrapper" padding={6}>
+      <TwitterTimelineEmbed
+        sourceType="timeline"
+        screenName={handle || "sawteenp"}
+        theme={theme}
+        tweetLimit={10}
+        noBorders={true}
+        noScrollbar={true}
+        transparent={true}
+        ariaPolite="assertive"
+        placeholder="Track SAWTEE"
+        options={{ height: height, width: `${width + "%"}` }}
+      />
+    </TwitterWrapper>
   );
 };
 
