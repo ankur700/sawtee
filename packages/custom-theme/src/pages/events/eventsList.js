5e33afe531a7182de22de32d243cb8599891211b
Ankur Singh <abishek.singh.chauhan@gmail.com>
2023-06-08 23:50:48 +0545

    added pulseloading component to eventlist

--- packages/custom-theme/src/pages/events/eventsList.js
+++ packages/custom-theme/src/pages/events/eventsList.js
@@ -1,4 +1,4 @@
-import {useMemo} from "react";
+import { useEffect, useState } from "react";
 import {
   Box,
   HStack,
@@ -10,20 +10,18 @@ import {
   useColorModeValue,
   Heading,
 } from "@chakra-ui/react";
-import moment from "moment/moment";
 import { PostImageWithOverlay } from "../../components/molecules/featured-post/components";
-import { motion } from "framer-motion";
 // Here we have used react-icons package for the icons
 import { GoChevronRight } from "react-icons/go";
 import Link from "../../components/atoms/link";
 import { decode, connect } from "frontity";
 import { formatCPTData, formatDateWithMoment } from "../../components/helpers";
 import PostCategories from "../../components/organisms/post/post-categories";
+import PulseLoadingCards from "../../components/atoms/pulseLoadingCards";
 
 const EventsList = ({ state, link, libraries, linkColor, categories }) => {
   const format = "MMMM Do YYYY";
   const Html2React = libraries.html2react.Component;
-  const MotionStack = motion(Stack);
   const StackBackground = useColorModeValue(
     "rgba(255, 255, 255, 0.1)",
     "rgba(0, 0, 0, 0.4)"
@@ -40,130 +38,127 @@ const EventsList = ({ state, link, libraries, linkColor, categories }) => {
     "rgb(88,175,223,.1)"
   );
   const data = state.source.get(link);
-  const events = useMemo(() => {
-    let array = [];
-    data.items.map((item) => {
-      const post = state.source[item.type][item.id];
-      array.push(formatCPTData(state, post, categories));
-    });
-    if (array.length > 0) {
-      return [...array];
-    }
+  const [events, setEvents] = useState([]);
+  console.log("🚀 ~ file: eventsList.js:42 ~ EventsList ~ events:", events);
+  useEffect(() => {
+    data.isReady &&
+      data.items.map((item) => {
+        const post = state.source[item.type][item.id];
+        post &&
+          setEvents((prev) => [
+            ...prev,
+            formatCPTData(state, post, categories),
+          ]);
+      });
   }, [data]);
   return (
     <VStack spacing={8} w={{ base: "auto", md: "full" }} maxW={"3xl"}>
-      {events
-        ? events.map((event, idx) => (
-            <MotionStack
-              direction="column"
-              spacing={4}
-              role="group"
-              key={event.id}
-              w="full"
-              overflow={"hidden"}
-              rounded="xl"
-              bg={StackBackground}
-              boxShadow={StackBoxShadow}
-              _hover={{
-                border: "1px solid",
-                borderColor: "primary.700",
-                boxShadow: "none",
-              }}
-              initial={{ y: 200, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{
-                duration: 0.5,
-                delay: 0.2 * (idx + 1),
-                type: "spring",
-                bounce: 0.4,
-              }}
-            >
-              <Box overflow={"hidden"}>
-                {event.featured_media && (
-                  <Link link={event.link}>
-                    <PostImageWithOverlay
-                      {...event.featured_media}
-                      borderRadius="0.75rem 0.75rem 0 0"
-                      overflow="hidden"
-                      _groupHover={{
-                        transition: "transform 0.4s ease-in-out",
-                        transform: "scale(1.05)",
-                        borderRadius: "0.75rem 0.75rem 0 0",
-                      }}
+      {events.length > 0 ? (
+        events.map((event, idx) => (
+          <Stack
+            direction="column"
+            spacing={4}
+            role="group"
+            key={event.id}
+            w="full"
+            overflow={"hidden"}
+            rounded="xl"
+            bg={StackBackground}
+            boxShadow={StackBoxShadow}
+            _hover={{
+              border: "1px solid",
+              borderColor: "primary.700",
+              boxShadow: "none",
+            }}
+          >
+            <Box overflow={"hidden"}>
+              {Object.entries(event.featured_media).length > 0 && (
+                <Link link={event.link}>
+                  <PostImageWithOverlay
+                    {...event.featured_media}
+                    borderRadius="0.75rem 0.75rem 0 0"
+                    overflow="hidden"
+                    _groupHover={{
+                      transition: "transform 0.4s ease-in-out",
+                      transform: "scale(1.05)",
+                      borderRadius: "0.75rem 0.75rem 0 0",
+                    }}
+                  />
+                </Link>
+              )}
+            </Box>
+            <Box py={4} px={8}>
+              <HStack spacing={2} mb={1}>
+                {event.categories && (
+                  <Flex justifyContent="space-between" alignItems="center">
+                    <PostCategories
+                      justify="flex-start"
+                      categories={event.categories}
+                      color={categoriesColor}
                     />
-                  </Link>
+                  </Flex>
                 )}
+              </HStack>
+              <Box textAlign="left">
+                <Heading
+                  as="h3"
+                  color={headingColor}
+                  fontSize={{ base: "md", md: "lg", lg: "xl" }}
+                  fontWeight="bold"
+                  _hover={{ color: linkColor, textDecoration: "underline" }}
+                >
+                  <Link w="100%" link={event.link ? event.link : "#"}>
+                    {decode(event.title)}
+                  </Link>
+                </Heading>
+                <Text
+                  as="div"
+                  fontSize={{ base: "sm", lg: "md" }}
+                  color={textColor}
+                  noOfLines={2}
+                  lineHeight="normal"
+                >
+                  <Html2React html={event.excerpt} />
+                </Text>
               </Box>
-              <Box p={4}>
-                <HStack spacing={2} mb={1}>
-                  {event.categories && (
-                    <Flex justifyContent="space-between" alignItems="center">
-                      <PostCategories
-                        justify="flex-start"
-                        categories={event.categories}
-                        color={categoriesColor}
-                      />
-                    </Flex>
-                  )}
-                </HStack>
-                <Box textAlign="left">
-                  <Heading
-                    as="h3"
-                    color={headingColor}
-                    fontSize={{ base: "md", md: "lg", lg: "xl" }}
-                    fontWeight="bold"
-                    _hover={{ color: linkColor, textDecoration: "underline" }}
-                  >
-                    <Link w="100%" link={event.link ? event.link : "#"}>
-                      {decode(event.title)}
-                    </Link>
-                  </Heading>
-                  <Text
-                    as="div"
-                    fontSize={{ base: "sm", lg: "md" }}
-                    color={textColor}
-                    noOfLines={2}
-                    lineHeight="normal"
-                  >
-                    <Html2React html={event.excerpt} />
+              <Box mt="2">
+                <Stack
+                  justify="space-between"
+                  direction={{ base: "column", sm: "row" }}
+                  alignItems={"center"}
+                >
+                  <Text fontSize="sm" color={textColor}>
+                    {formatDateWithMoment(event.publishDate, format)}
                   </Text>
-                </Box>
-                <Box mt="2">
-                  <Stack
-                    justify="space-between"
-                    direction={{ base: "column", sm: "row" }}
-                    alignItems={"center"}
+                  <HStack
+                    spacing={1}
+                    p={1}
+                    px={2}
+                    alignItems="center"
+                    height="2rem"
+                    w="max-content"
+                    margin="auto 0"
+                    rounded="md"
+                    // color={useColorModeValue("primary.700", "primary.50")}
+                    link={event.link ? event.link : "#"}
+                    _hover={{
+                      bg: readMoreButtonColor,
+                    }}
                   >
-                    <Text fontSize="sm" color={textColor}>
-                      {formatDateWithMoment(event.publishDate, format)}
-                    </Text>
-                    <HStack
-                      spacing={1}
-                      p={1}
-                      px={2}
-                      alignItems="center"
-                      height="2rem"
-                      w="max-content"
-                      margin="auto 0"
-                      rounded="md"
-                      // color={useColorModeValue("primary.700", "primary.50")}
-                      link={event.link ? event.link : "#"}
-                      _hover={{
-                        bg: readMoreButtonColor,
-                      }}
-                    >
-                      <Link link={event.link}>
-                        {" "}
-                        <Text fontSize="sm"> Read more</Text>
-                      </Link>
-                      <Icon as={GoChevronRight} w={4} h={4} />
-                    </HStack>
-                  </Stack>
-                </Box>
+                    <Link link={event.link}>
+                      {" "}
+                      <Text fontSize="sm"> Read more</Text>
+                    </Link>
+                    <Icon as={GoChevronRight} w={4} h={4} />
+                  </HStack>
+                </Stack>
               </Box>
-            </MotionStack>
-          ))
-        : null}
+            </Box>
+          </Stack>
+        ))
+      ) : (
+        <PulseLoadingCards />
+      )}
     </VStack>
   );
 };
