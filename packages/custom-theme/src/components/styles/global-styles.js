5efc2be2ec69b23993cb672b3671510508e2e530
Ankur Singh <abishek.singh.chauhan@gmail.com>
2023-06-13 17:54:33 +0545

    Removed bloated css declrations

--- packages/custom-theme/src/components/styles/global-styles.js
+++ packages/custom-theme/src/components/styles/global-styles.js
@@ -13,63 +13,51 @@ import gutenbergTheme from "./theme.js";
  */
 const documentSetup = (colors) => css`
   html {
-    // font-size: 62.5%; /* 1rem = 10px */
     scroll-behavior: smooth;
   }
 
   html {
-  --scrollbarBG: #CFD8DC;
-  --thumbBG: #90A4AE;
-}
-body::-webkit-scrollbar {
-  width: 11px;
-}
-body {
-  scrollbar-width: thin;
-  scrollbar-color: var(--thumbBG) var(--scrollbarBG);
-}
-body::-webkit-scrollbar-track {
-  background: var(--scrollbarBG);
-}
-body::-webkit-scrollbar-thumb {
-  background-color: var(--thumbBG) ;
-  border-radius: 6px;
-  border: 3px solid var(--scrollbarBG);
-}
-  .chakra-ui-light {
-    background: ${colors.bodyBg.light};
+    --scrollbarBG: #cfd8dc;
+    --thumbBG: #90a4ae;
   }
-  .chakra-ui-dark {
-    background: ${colors.bodyBg.dark};
-  }
-
-  .chakra-ui-dark .twitter-timeline > .r-kemksi {
-    background: rgba(0, 0, 0, 0.3) !important;
-  }
-
-  .twitter-timeline > iframe {
-    border-radius: 10px;
-  }
-
-
-
 
   body {
     box-sizing: border-box;
     background-color: #ffffff;
     color: #121212;
-    font-family: "Open Sans", -apple-system, BlinkMacSystemFont, "Source Sans Pro", sans-serif;
-      Helvetica, sans-serif;
+    font-family: var(--chakra-fonts-body);
     letter-spacing: -0.015em;
     line-height: 1.5;
     text-align: left;
     font-weight: normal;
-
-    button,
-    span {
-      // font-family: "Open Sans", monospace;
-    }
   }
+
+  body::-webkit-scrollbar {
+    width: 11px;
+  }
+
+  body {
+    scrollbar-width: thin;
+    scrollbar-color: var(--thumbBG) var(--scrollbarBG);
+  }
+
+  body::-webkit-scrollbar-track {
+    background: var(--scrollbarBG);
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: var(--thumbBG);
+    border-radius: 6px;
+    border: 3px solid var(--scrollbarBG);
+  }
+
+  // .chakra-ui-light {
+  //   background: ${colors.bodyBg.light};
+  // }
+
+  // .chakra-ui-dark {
+  //   background: ${colors.bodyBg.dark};
+  // }
 
   *,
   *::before,
@@ -102,13 +90,12 @@ body::-webkit-scrollbar-thumb {
     color: rgb(20 158 202/1);
   }
 
-
-@media (hover: none) and (pointer: coarse) {
-    .left-arrow, .right-arrow {
-        display: none;
+  @media (hover: none) and (pointer: coarse) {
+    .left-arrow,
+    .right-arrow {
+      display: none;
     }
-}
-
+  }
 `;
 
 const accessibilitySettings = css`
